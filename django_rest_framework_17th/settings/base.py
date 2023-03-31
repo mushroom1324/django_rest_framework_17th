1a72dbbba8c46611866ad1f30179bf70d4511a72
seochanhyeok <mushroom1324@naver.com>
2023-03-31 21:18:23 +0900

    FIX: configure DEFAULT_AUTO_FIELD

--- django_rest_framework_17th/settings/base.py
+++ django_rest_framework_17th/settings/base.py
@@ -116,3 +116,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
