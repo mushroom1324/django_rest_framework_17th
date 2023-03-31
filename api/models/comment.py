7639a450b38bd40435f545eab43e993245be319d
seochanhyeok <mushroom1324@naver.com>
2023-03-31 21:16:58 +0900

    FEAT: implement Comment model

---
+++ api/models/comment.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from api.models.post import Post
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.PositiveIntegerField(default=0)
