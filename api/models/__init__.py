df4bc1b825850b8982051bae2bab6312bb77c682
seochanhyeok <mushroom1324@naver.com>
2023-04-01 17:30:41 +0900

    FEAT: implement init.py and import models

---
+++ api/models/__init__.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .user_profile import User, UserProfile
+from .post import Post
+from .comment import Comment
+from .reply import Reply
+from .category import Category
+from .user_subject import UserSubject
+from .subject import Subject
+from .category import Category
+from .subject_review import SubjectReview
+
+#  Remember to import models!!
