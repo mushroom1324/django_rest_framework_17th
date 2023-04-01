12b60e0a73156a75db14e17e790191e3948e628c
seochanhyeok <mushroom1324@naver.com>
2023-04-01 16:22:04 +0900

    FEAT: make new model subject_review

---
+++ api/models/subject_review.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from api.models.subject import Subject
+
+
+class SubjectReview(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    content = models.TextField()
+    rate = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
