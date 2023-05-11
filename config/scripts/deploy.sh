650260177c89dcee026380b31f5b32eb75d9502b
seochanhyeok <mushroom1324@naver.com>
2023-05-12 00:39:08 +0900

    REFACTOR : Pillow 버전충돌

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -22,6 +22,9 @@ then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
-
+sudo apt-get install python3-dev python3-setuptools
+sudo apt-get install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev \
+    libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk \
+    libharfbuzz-dev libfribidi-dev libxcb1-dev
 echo "start docker-compose up: ubuntu"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.prod.yml up --build -d
